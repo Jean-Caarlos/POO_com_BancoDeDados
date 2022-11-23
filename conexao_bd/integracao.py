c106a4c548254764175a08f1dd599d403eaccf4c
Jean Carlos Morais <jean_morais2000@hotmail.com>
2022-11-23 18:03:29 -0300

    Update integracao.py

--- conexao_bd/integracao.py
+++ conexao_bd/integracao.py
@@ -1,10 +1,10 @@
 import pyodbc
 
 # CONECTANDO O SQL COM O PYTHON
-server = 'svr-estudos-v2.database.windows.net'
-database = 'adb-estudos-v2'
-username = 'jean.amorim@blueshift.com.br'
-driver = '{ODBC Driver 18 for SQL Server}'
+server = 'server'
+database = 'database'
+username = 'username@blueshift.com'
+driver = '{ODBC Driver for SQL Server}'
 Authentication = 'ActiveDirectoryInteractive'
 conexao = pyodbc.connect(
 'DRIVER=' + driver + ';SERVER=' + server + ';AUTHENTICATION=' + Authentication + ';DATABASE=' + database + ';UID=' + username)
